e18e697b0546f32b560625119ed732254f9f70d7
ItzJuliBG <71836116+ItzJuliBG@users.noreply.github.com>
2023-05-17 19:47:16 +0300

    Basic Syntax

---
+++ Basic Syntax, Conditional Statements and Loops Exercise/07. Vending Machine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._Vending_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            while (true)
+            {
+                string coinInput = Console.ReadLine();
+                if (coinInput == "Start")
+                {
+                    break;
+                }
+                else
+                {
+                    sum += Double.Parse(coinInput);
+                   
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
