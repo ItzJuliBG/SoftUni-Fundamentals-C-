10982509db34856f711768605b5e6739bb3083d6
ItzJuliBG <71836116+ItzJuliBG@users.noreply.github.com>
2023-06-05 23:44:18 +0300

    6/5/2023

---
+++ 12. Exercise Arrays/01. Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonCount = int.Parse(Console.ReadLine());
+
+            string[] people = new string[wagonCount];
+            int sum = 0;
+            for (int i = 0; i < wagonCount; i++)
+            {
+                people[i] = Console.ReadLine();
+              //  sum += people[i];
+            }
+            foreach (var item in people)
+            {
+                Console.Write($"{item.ToString()} ");
+            }
+            Console.WriteLine();
+           // Console.WriteLine(sum);
+            //Console.WriteLine(string.Join(" ", people));
+        }
+    }
+}
---
+++ 12. Exercise Arrays/02. Common Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(" ");
+            string[] secondArray = Console.ReadLine().Split(" ");
+
+            foreach (string a in secondArray)
+            {
+                foreach (string b in firstArray)
+                {
+                    if (a == b)
+                    {
+                        Console.Write($"{b} ");
+                    }
+                }
+            }
+
+        }
+    }
+}
