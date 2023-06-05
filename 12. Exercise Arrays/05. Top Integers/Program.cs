bec8b5a2fd161c8a7f68e9b9e203d12525839640
ItzJuliBG <71836116+ItzJuliBG@users.noreply.github.com>
2023-06-06 00:59:18 +0300

    5/6/2023

---
+++ 12. Exercise Arrays/05. Top Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int j = 0;
+
+            int[] topNums = new int[] { };
+            for (int i = 0; i < input.Length; i++)
+            {
+                //if (i == j-1)
+                //{
+                //    break;
+                //}
+                j = i + 1;
+                if (input[i] > input[j])
+                {
+                    // input[i] = topNums[i];
+                }
+            }
+            Console.WriteLine(string.Join(" ", topNums));
+
+
+
+        }
+    }
+}
--- 12. Exercise Arrays/07. Max Sequence of Equal Elements/Program.cs
+++ 12. Exercise Arrays/07. Max Sequence of Equal Elements/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
 
 namespace _07._Max_Sequence_of_Equal_Elements
 {
@@ -6,7 +9,33 @@ namespace _07._Max_Sequence_of_Equal_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int currentNum = 0;
+            int previousNum = input[0];
+            int currentSeq = 0;
+            int longestSeqCount = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        currentSeq++;
+                        if (currentSeq > longestSeqCount)
+                        {
+                            longestSeqCount = currentSeq;
+                        }
+                    }
+                    else
+                    {
+                        currentSeq = 0;
+                    }
+                }
+            }
+            Console.WriteLine(longestSeqCount);
         }
     }
 }
